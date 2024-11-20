2fee07546770170f97fc01158b856272593402d7
Rafael Saca <raffaellsaca@gmail.com>
2024-11-20 16:18:37 -0300

    feat: Tela de login

---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+
+
+}
