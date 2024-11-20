2940126e11f707a855bd5852ed1e3fc889c4c2bb
Rafael Saca <raffaellsaca@gmail.com>
2024-11-19 22:45:53 -0300

    feat: Service de Produtos - CRUD

---
+++ src/app/service/produto.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Produto } from '../models/produto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  private apiUrl= 'http://localhost:8080/api/produtos';
+
+  constructor(private http: HttpClient) { }
+
+  cadastrar(produto: Produto){
+    return this.http.post<Produto>(this.apiUrl,produto);
+
+  }
+
+  listar(){
+    return this.http.get<Produto[]>(this.apiUrl);
+  }
+
+  editar(id: string, produto: Partial<Produto>){
+    return this.http.put<Produto>(`${this.apiUrl}/${id}`, produto);
+  }
+
+  deletar(id: string){
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  buscaPorId(id: string){
+    return this.http.get<Produto>(`${this.apiUrl}/${id}`);
+  }
+}
