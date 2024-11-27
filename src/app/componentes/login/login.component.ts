c9ef98e6d65c343dc7f8a2f711315d248e8d86c9
Rafael Saca <raffaellsaca@gmail.com>
2024-11-27 20:32:53 -0300

    feat: Login - lógica para enviar os dados do usuário e receber o token

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,35 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  errorMsg: string | null = null;
 
+  constructor( private fb: FormBuilder, private authService: AuthService, private router: Router){
+    this.loginForm = this.fb.group({
+      username:['', [Validators.required, Validators.email]],
+      password:['', Validators.required]
+    });
+  }
+
+  onSubmit(){
+    if (this.loginForm.valid){
+      const{ username, password} = this.loginForm.value;
+
+      this.authService.login(username, password).subscribe({
+        next: (response) => {
+          console.log('login bem sucedido');
+          const token = response.acess_token;
+          this.authService.setToken(token);
+          console.log('outra pagina')
+          this.router.navigate(['estoque']);
+        },
+
+        error: (error) => {
+          this.errorMsg = 'Dados inválidos';
+          console.log('Erro no login', error);
+        }
+      });
+    }
+  }
 
 
 }
