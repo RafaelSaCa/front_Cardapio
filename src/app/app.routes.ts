da81004dbd599856419d038ea5734ea55470d0dd
Rafael Saca <raffaellsaca@gmail.com>
2024-11-27 20:41:37 -0300

    refactor: Guarda de rotas - impede que usuários não autorizados acessem determinadas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { CardBebidasComponent } from './bebidas/card-bebidas/card-bebidas.component';
 import { CadastroProdutoComponent } from './componentes/cadastro/cadastro-produto/cadastro-produto.component';
+import { LoginComponent } from './componentes/login/login.component';
 import { TabelaProdutoComponent } from './componentes/tabela-produto/tabela-produto.component';
 import { CardComponent } from './lanches/card/card.component';
 import { CardPizzasComponent } from './pizzas/card-pizzas/card-pizzas.component';
 import { CardSorvetesComponent } from './sorvetes/card-sorvetes/card-sorvetes.component';
-import { LoginComponent } from './componentes/login/login.component';
+import { authGuard } from './service/auth-guard.service';
 
 export const routes: Routes = [
   {
@@ -13,34 +14,37 @@ export const routes: Routes = [
     component:LoginComponent,
   },
   {
-    path:'**', redirectTo: 'login'
-  },
-  {
     path: '',
     component: CardComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'bebidas',
     component: CardBebidasComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'pizzas',
     component: CardPizzasComponent,
+    canActivate: [authGuard]
   },
 
   {
     path: 'sorvetes',
     component: CardSorvetesComponent,
+    canActivate: [authGuard]
   },
 
   {
     path: 'cadastroProduto',
     component: CadastroProdutoComponent,
+    canActivate: [authGuard]
   },
 
   {
     path: 'estoque',
     component: TabelaProdutoComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'produtos/editar/:id',
