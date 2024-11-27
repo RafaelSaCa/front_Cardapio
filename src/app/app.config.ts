59f809dc3e42416cbcdb169995668336026d4133
Rafael Saca <raffaellsaca@gmail.com>
2024-11-27 20:38:59 -0300

    feat: Provider HttpClient withFetch e AuthInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
+import { routes } from './app.routes';
+import { AuthInterceptor } from './service/auth.interceptor';
+
 
 ToastrModule.forRoot({
   timeOut: 10000,
@@ -16,8 +18,12 @@ export const appConfig: ApplicationConfig = {
 
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
     provideToastr(),
+
+
+
   ],
 };
