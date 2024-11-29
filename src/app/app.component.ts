136018ebf48393da18aa188365620564980c55b8
Rafael Saca <raffaellsaca@gmail.com>
2024-11-28 22:03:42 -0300

    feat: Verifica se usuario logado para exibir navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
+import { HeaderComponent } from './componentes/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { HeaderComponent } from './componentes/header/header.component';
+import { AuthService } from './service/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cardapio';
+
+  constructor(private authService : AuthService){}
+
+  isLoggedIn(): boolean {
+    return this.authService.isLogged();
+  }
 }
